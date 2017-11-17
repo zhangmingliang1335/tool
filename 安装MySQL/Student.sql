43bd70168934ea5310edab2e2a11836c0bd94c5e
zml <893786407@qq.com>
2017-11-17 22:48:14 +0800

    tool

---
+++ 安装MySQL/Student.sql
@@ -0,0 +1,17 @@
+create table student(
+	id int,
+	name varchar(20),
+	chinese float,
+	english float,
+	math float
+);
+
+insert into student(id,name,chinese,english,math) values(1,'张小明',89,78,90);
+insert into student(id,name,chinese,english,math) values(2,'李进',67,98,56);
+insert into student(id,name,chinese,english,math) values(3,'王五',87,78,77);
+insert into student(id,name,chinese,english,math) values(4,'李一',88,98,90);
+insert into student(id,name,chinese,english,math) values(5,'李来财',82,84,67);
+insert into student(id,name,chinese,english,math) values(6,'张进宝',55,85,45);
+insert into student(id,name,chinese,english,math) values(7,'黄蓉',75,65,30);
+
+
